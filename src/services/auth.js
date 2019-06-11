181339460dc90e961568917390af07e42824c660
Payton Lower <paytown@me.com>
2019-06-11 15:19:51 -0700

    updated auth and eslint

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,9 @@
 import { WebAuth } from 'auth0-js';
 
 const auth0 = new WebAuth({
-  domain: 'paytown.auth0.com',
-  clientID: 'IO2LsCwzTteaU0Fjx1E3rogmqRJh332V',
-  redirectUri: 'http://localhost:7890/callback',
+  domain: process.env.AUTH0_DOMAIN,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  redirectUri: process.env.AUTH0_CALLBACK,
   responseType: 'token id_token',
   scope: 'openid profile'
 });
